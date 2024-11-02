b997c8d2580f655456815b68f2c835026b431b03
therosenstock <fabiolars2009@hotmail.com>
2024-11-02 17:41:12 -0300

    menu

---
+++ app/src/main/java/com/example/atletas/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.atletas;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    /*
+     *@author:<Fabiola Rodrigues dos Santos / RA: 1110482313011>
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if( id == R.id.act1){
+            Intent i = new Intent(this, MainActivity.class);
+            this.startActivity(i);
+            this.finish();
+            return true;
+        }
+        if( id == R.id.act2){
+            Intent i = new Intent(this, MainActivity2.class);
+            this.startActivity(i);
+            this.finish();
+
+            return true;
+        }
+        if( id == R.id.act3){
+            Intent i = new Intent(this, MainActivity3.class);
+            this.startActivity(i);
+            this.finish();
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
